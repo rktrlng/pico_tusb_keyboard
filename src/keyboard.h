85221a60f598ffa9f8be815160a48d5635356492
rktrlng <rik.teerling@gmail.com>
2022-04-19 20:07:40 +0200

    mame keys

--- src/keyboard.h
+++ src/keyboard.h
@@ -23,19 +23,34 @@ class KeyBoard
 private:
 	// ===========================================================================
 	// set these values to your situation
-	const static int num_pins = 11; // max 32 pins (state is uint32_t)
+	const static int num_pins = 26; // max 32 pins (state is uint32_t)
 	const PinKey pin_keys[num_pins] = { // connect gpio pin to keycode
-		{ 2, HID_KEY_W },
-		{ 3, HID_KEY_A },
-		{ 4, HID_KEY_S },
-		{ 5, HID_KEY_D },
-		{ 16, HID_KEY_ARROW_UP },
-		{ 17, HID_KEY_ARROW_DOWN },
-		{ 18, HID_KEY_ARROW_LEFT },
-		{ 19, HID_KEY_ARROW_RIGHT },
-		{ 20, HID_KEY_ENTER },
-		{ 21, HID_KEY_SPACE },
-		{ 22, HID_KEY_SHIFT_LEFT }
+		{ 0, HID_KEY_1 }, // 1 player
+		{ 1, HID_KEY_5 }, // coin slot 1
+		{ 2, HID_KEY_ARROW_UP },
+		{ 3, HID_KEY_ARROW_DOWN },
+		{ 4, HID_KEY_ARROW_LEFT },
+		{ 5, HID_KEY_ARROW_RIGHT },
+		{ 6, HID_KEY_E }, // up (left stick)
+		{ 7, HID_KEY_D }, // down (left stick)
+		{ 8, HID_KEY_S }, // left (left stick)
+		{ 9, HID_KEY_F }, // right (left stick)
+		{ 10, HID_KEY_I }, // up (right stick)
+		{ 11, HID_KEY_K }, // down (right stick)
+		{ 12, HID_KEY_J }, // left (right stick)
+		{ 13, HID_KEY_L }, // right (right stick)
+		{ 14, HID_KEY_CONTROL_LEFT }, // button 1
+		{ 15, HID_KEY_ALT_LEFT }, // button 2
+		{ 16, HID_KEY_SPACE }, // button 3
+		{ 17, HID_KEY_SHIFT_LEFT }, // button 4
+		{ 18, HID_KEY_Z }, // button 5
+		{ 19, HID_KEY_X }, // button 6
+		{ 20, HID_KEY_C }, // button 7
+		{ 21, HID_KEY_V },  // button 8
+		{ 22, HID_KEY_ENTER }, // select
+		{ 26, HID_KEY_ESCAPE }, // back
+		{ 27, HID_KEY_SHIFT_RIGHT },
+		{ 28, HID_KEY_BACKSPACE }
 	};
 	// ===========================================================================
 
